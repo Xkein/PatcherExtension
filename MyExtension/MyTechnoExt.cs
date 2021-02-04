4063bb50b8b7ce0386180869b3b33a46bc43c260
Xkein <739502808@qq.com>
2021-02-05 01:51:02 +0800

    add a sample version of extra extension.

---
+++ MyExtension/MyTechnoExt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension.Ext
+{
+    public partial class TechnoExt
+    {
+        public string MyExtensionTest = nameof(MyExtensionTest);
+    }
+}
