1552cdfa329f633990ef80a4b89db1c38513c589
Xkein <739502808@qq.com>
2021-02-26 02:18:22 +0800

    add partial manage sample to use multi-extension.

--- MyExtension/MyTechnoExt.cs
+++ MyExtension/MyTechnoExt.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Extension.Utilities;
+using PatcherYRpp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +11,21 @@ namespace Extension.Ext
     public partial class TechnoExt
     {
         public string MyExtensionTest = nameof(MyExtensionTest);
+    }
+
+    [INILoadAction("LoadINI")]
+    public partial class TechnoTypeExt
+    {
+        [NonSerialized]
+        public Pointer<SuperWeaponTypeClass> FireSuperWeapon;
+
+
+        public void LoadINI(Pointer<CCINIClass> pINI)
+        {
+            INIReader reader = new INIReader(pINI);
+            string section = OwnerObject.Ref.Base.Base.GetID();
+
+            reader.ReadSuperWeapon(section, nameof(FireSuperWeapon), ref FireSuperWeapon);
+        }
     }
 }
