39fd7e1950ddbf1f2e847c0df156fae0b96722b5
Xkein <739502808@qq.com>
2022-03-06 13:33:18 +0800

    component system.

---
+++ Components/ExtComponent.cs
@@ -0,0 +1,47 @@
+﻿using Extension.Ext;
+using Extension.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension.Components
+{
+    [Serializable]
+    public class ExtComponent<TExt> : Component where TExt : class, IExtension
+    {
+        public ExtComponent(TExt owner, int id, string name) : base(id)
+        {
+            _owner = owner;
+            Name = name;
+        }
+
+        public TExt Owner => _owner;
+        public event Action OnAwake;
+
+        public override void Awake()
+        {
+            base.Awake();
+
+            _awaked = true;
+            OnAwake?.Invoke();
+            ForeachChild(c => c.Awake());
+        }
+
+        /// <summary>
+        /// return myself and ensure awaked
+        /// </summary>
+        /// <returns></returns>
+        public ExtComponent<TExt> GetEnsureAwaked()
+        {
+            if (!_awaked)
+                Awake();
+
+            return this;
+        }
+
+        bool _awaked = false;
+        ExtensionReference<TExt> _owner;
+    }
+}
---
+++ Components/Transform.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension.Components
+{
+    [Serializable]
+    public class Transform : Component
+    {
+        public Transform() : base(true)
+        {
+            _transform = this;
+        }
+
+        public virtual Vector3 Location { get; set; }
+        public virtual Vector3 Rotation { get; set; }
+        public virtual Vector3 Scale { get; set; }
+
+        public Transform GetParent()
+        {
+            return Parent.Parent.Transform;
+        }
+
+        public new Transform GetRoot()
+        {
+            return base.GetRoot().Transform;
+        }
+    }
+}
